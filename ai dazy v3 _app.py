a97a4a5039b890d5d7ff96ecb8e3e0c945a500cb
dantejwook <raypath001@gmail.com>
2025-12-18 18:31:16 +0900

    Update ai dazy v3 _app.py

--- ai dazy v3 _app.py
+++ ai dazy v3 _app.py
@@ -139,13 +139,13 @@ st.sidebar.markdown(
 col1, col2 = st.sidebar.columns(2)
 
 with col1:
-   if st.sidebar.button("🧹 Cache Reset"):
+   if st.button("🧹 Cache Reset"):
        reset_cache()
        st.sidebar.success("✅ Cache Reset is complete.")
        st.rerun()
 
 with col2:
-   if st.sidebar.button("🗑️ Download Reset"):
+   if st.button("🗑️ Download Reset"):
        reset_output()
        st.sidebar.success("✅ Download Reset is complete.")
        st.rerun()
