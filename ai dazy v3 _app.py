626e9e27680ec898bf15804d8bf3f8a6b72bb3a7
dantejwook <raypath001@gmail.com>
2025-12-19 03:43:29 +0900

    Update ai dazy v3 _app.py

--- ai dazy v3 _app.py
+++ ai dazy v3 _app.py
@@ -282,10 +282,6 @@ with left_col:
         type=["md", "pdf", "txt"],
         key=f"uploader_{st.session_state.uploader_key}",
     )
-
-col1, col2 = st.columns([1, 1], gap="small")
-    )
-
     # ✅ 반드시 여기 안에서
     col1, col2 = st.columns([1, 1], gap="small")
 
